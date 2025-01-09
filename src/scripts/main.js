cc7c6440e2b170a874e5864508c5d61c7f88ac69
Albina <yakunina.albina@gmail.com>
2025-01-09 12:42:29 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rowsIndex = 1;
+const rowsReplace = 4;
+
+const rows = table.rows;
+
+for (let i = 0; i < rows.length; i++) {
+  const cell = rows[i].cells[rowsIndex];
+  const cellClone = cell.cloneNode(true);
+  const newCell = rows[i].insertCell(rowsReplace);
+
+  newCell.appendChild(cellClone);
+}
